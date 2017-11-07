97a167f8dacd5edf140b6ef978f473c52f73d622
xiaody <xdy@zhihu.com>
2017-11-07 14:09:59 +0800

    Improve loose version

--- src/loose.js
+++ src/loose.js
@@ -1,25 +1,28 @@
 const React = require('react')
 
 function LinesEllipsisLoose (props) {
-  const {component: Component, text, lineHeight, maxLine, style, ...rest} = props
-  const lineHeightNumber = parseInt(lineHeight, 10)
-  const unit = typeof lineHeight === 'string' && lineHeight.trim().endsWith('em')
-    ? 'em'
-    : 'px'
+  const {component: Component, text, lineHeight, maxLine, style, overflowFallback, ...rest} = props
   const maxLineNumber = +maxLine || 1
+  let usedStyle = {
+    ...style,
+    display: '-webkit-box',
+    WebkitBoxOrient: 'vertical',
+    WebkitLineClamp: maxLineNumber
+  }
+  if (overflowFallback && lineHeight) {
+    const lineHeightNumber = parseFloat(lineHeight)
+    const unit = typeof lineHeight === 'string' && lineHeight.trim().endsWith('em')
+      ? 'em'
+      : 'px'
+    usedStyle = {
+      ...usedStyle,
+      lineHeight: `${lineHeightNumber}${unit}`,
+      maxHeight: `${maxLineNumber * lineHeightNumber}${unit}`,
+      overflow: 'hidden'
+    }
+  }
   return (
-    <Component
-      {...rest}
-      style={{
-        ...style,
-        lineHeight: `${lineHeightNumber}${unit}`,
-        maxHeight: `${maxLineNumber * lineHeightNumber}${unit}`,
-        overflow: 'hidden',
-        display: '-webkit-box',
-        WebkitBoxOrient: 'vertical',
-        WebkitLineClamp: maxLineNumber
-      }}
-    >
+    <Component {...rest} style={usedStyle}>
       {text}
     </Component>
   )
@@ -28,7 +31,8 @@ function LinesEllipsisLoose (props) {
 LinesEllipsisLoose.defaultProps = {
   component: 'div',
   maxLine: 1,
-  style: {}
+  style: {},
+  overflowFallback: true
 }
 
 module.exports = LinesEllipsisLoose
