83e1301510960b6a5bb26f831fca2ae9b3621516
Shoghy Martinez <shoghymartinez@gmail.com>
2024-03-13 16:48:45 -0400

    HTMLEllipsis types

---
+++ src/html.d.ts
@@ -0,0 +1,26 @@
+declare module "react-lines-ellipsis/lib/html" {
+  export interface HTMLEllipsisProps<T extends StrComponent>{
+    component?: T
+    unsafeHTML?: string
+    maxLine?: number
+    ellipsis?: string
+    ellipsisHTML?: string
+    className?: string
+    basedOn?: "letters" | "words"
+  }
+
+  export type HTMLEllipsisExtendedProps<T extends StrComponent> = JSX.IntrinsicElements[T] & HTMLEllipsisProps<T>;
+  
+  export interface HTMLEllipsisState{
+    html: string | undefined
+    clamped: boolean
+  }
+  
+  export default class HTMLEllipsis<T extends StrComponent = 'div'> extends React.Component<
+    HTMLEllipsisExtendedProps<T>,
+    HTMLEllipsisState,
+    unknown
+  >{
+    static defaultProps: HTMLEllipsisProps<'div'>;
+  }
+}
